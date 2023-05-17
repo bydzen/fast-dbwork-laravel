ba30ff3cbcbb07c62cc2073ba4ef4c2f9cc7ccce
bydzen <svnteentthones@gmail.com>
2023-05-17 13:57:32 +0700

    title change and main class

--- resources/views/layout/template.blade.php
+++ resources/views/layout/template.blade.php
@@ -4,13 +4,13 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Data Mahasiswa</title>
+    <title>Data of Students</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
 </head>
 
 <body class="bg-light">
-    <main class="container">
+    <main class="container min-vh-100 d-flex flex-column justify-content-center">
         @include('component.message')
         @yield('content')
     </main>
